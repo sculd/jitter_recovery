e889887e8bc6930307533385e56768af929ef9ca
Hyungjun Lim <sculd3@gmail.com>
2024-01-03 17:36:00 -0500

    initial telegram message fix

--- sandbox_live.py
+++ sandbox_live.py
@@ -24,7 +24,7 @@ import trading.price
 logging.info(f"### starting a new live at {datetime.datetime.now()}")
 
 import publish.telegram
-publish.telegram.post_message(f"### starting a new live at {datetime.datetime.now()}")
+publish.telegram.post_message(f"starting a new live at {datetime.datetime.now()}")
 
 price_cache = trading.price.PriceCache(trading_manager, 60)
 
