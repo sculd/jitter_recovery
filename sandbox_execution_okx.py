267d6a4bd857d52b09c68bfe1531de25bd70be42
Hyungjun Lim <sculd3@gmail.com>
2024-01-25 12:59:32 -0500

    sandbox_execution_okx.py

---
+++ sandbox_execution_okx.py
@@ -0,0 +1,26 @@
+import logging, sys, datetime, time
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s",
+    handlers=[
+        logging.StreamHandler(sys.stdout)
+    ]
+)
+
+from dotenv import load_dotenv
+
+load_dotenv()  # take environment variables from .env.
+
+import trading.execution_okx
+
+trade_execution = trading.execution_okx.TradeExecution(100, 5)
+
+
+trade_execution.enter('GMX-USDT-SWAP', 60, -1)
+
+
+time.sleep(3)
+
+trade_execution.exit('GMX-USDT-SWAP', 120)
+
