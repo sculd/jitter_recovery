dfe8dfe93937cfb3f1167ad519c25756bc3378a5
Hyungjun Lim <sculd3@gmail.com>
2024-02-01 21:22:49 -0500

    two main for short/long term binance

--- main_live_binance_longterm.py
+++ main_live_binance_longterm.py
@@ -5,7 +5,7 @@ logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s",
     handlers=[
-        logging.FileHandler("logs/{}.log".format("log_live_binance")),
+        logging.FileHandler("logs/{}.log".format("log_live_binance_longterm")),
         logging.StreamHandler(sys.stdout)
     ]
 )
@@ -17,15 +17,15 @@ load_dotenv()  # take environment variables from .env.
 import trading.trade, trading.execution_binance
 import trading.price_binance
 
-logging.info(f"### starting a new binance live at {datetime.datetime.now()}")
+logging.info(f"### starting a new binance longterm live at {datetime.datetime.now()}")
 
 import publish.telegram
-publish.telegram.post_message(f"starting a new binance live at {datetime.datetime.now()}")
+publish.telegram.post_message(f"starting a new binance longterm live at {datetime.datetime.now()}")
 
 trade_execution = trading.execution_binance.TradeExecution(target_betsize=50, leverage=5)
-trading_manager = trading.trade.TradeManager(is_long_term=False, trade_execution=trade_execution)
-logging.info("starting a binance shortterm")
-price_cache = trading.price_binance.PriceCache(trading_manager, trading_manager.trading_param.jitter_recover_feature_param.jump_window)
+trading_manager = trading.trade.TradeManager(is_long_term=True, trade_execution=trade_execution)
+logging.info("starting a binance longterm")
+price_cache_longterm = trading.price_binance.PriceCache(trading_manager, trading_manager.trading_param.jitter_recover_feature_param.jump_window)
 
 while True:
     trading_manager.trade_execution.print()
