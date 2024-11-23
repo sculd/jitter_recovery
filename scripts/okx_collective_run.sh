95e29e21b2513870a34f145e1a46d663a994e1ad
Hyungjun Lim <sculd3@gmail.com>
2024-11-23 10:41:34 -0500

    update script

--- scripts/okx_collective_run.sh
+++ scripts/okx_collective_run.sh
@@ -1,6 +1,8 @@
-cd /home/sculd3/projects/jitter_recovery/
+PROJECT_DIR="/home/$(whoami)/projects/jitter_recovery/"
+cd $PROJECT_DIR || exit
 LOG="logs/log_okx_collective_$(date +%Y-%m-%dT%H:%M:%S).txt"
-FILENAME="/home/sculd3/projects/jitter_recovery/main_live_okx_collective.py"
+FILENAME_LOCAL="main_live_okx_collective.py"
+FILENAME_FULL="${PROJECT_DIR}/${FILENAME_LOCAL}"
 PYTHONBIN="/home/sculd3/venvs/mlb/bin/python"
-RUN_COMMAND="echo $LOG && touch $LOG && date >> $LOG && echo $FILENAME >> $LOG && $PYTHONBIN $FILENAME >> $LOG 2>&1"
-pgrep -f $FILENAME || eval $RUN_COMMAND
+RUN_COMMAND="echo $LOG && touch $LOG && date >> $LOG && echo $FILENAME_LOCAL >> $LOG && $PYTHONBIN $FILENAME_FULL >> $LOG 2>&1"
+pgrep -f $FILENAME_LOCAL || eval $RUN_COMMAND
