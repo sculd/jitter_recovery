07731c8b28d59facd125bbfc187d0d50e6e82238
Hyungjun Lim <sculd3@gmail.com>
2024-11-23 12:06:58 -0500

    add a loop to the script to check periodically

--- scripts/okx_collective_run.sh
+++ scripts/okx_collective_run.sh
@@ -1,8 +1,15 @@
-PROJECT_DIR="/home/$(whoami)/projects/jitter_recovery/"
+PROJECT_DIR="/home/$(whoami)/projects/jitter_recovery"
 cd $PROJECT_DIR || exit
-LOG="logs/log_okx_collective_$(date +%Y-%m-%dT%H:%M:%S).txt"
+LOG="${PROJECT_DIR}/logs/log_okx_collective_$(date +%Y-%m-%dT%H:%M:%S).txt"
+LOG_PING="${PROJECT_DIR}/logs/ping.txt"
+touch $LOG_PING
 FILENAME_LOCAL="main_live_okx_collective.py"
 FILENAME_FULL="${PROJECT_DIR}/${FILENAME_LOCAL}"
 PYTHONBIN="/home/sculd3/venvs/mlb/bin/python"
 RUN_COMMAND="echo $LOG && touch $LOG && date >> $LOG && echo $FILENAME_LOCAL >> $LOG && $PYTHONBIN $FILENAME_FULL >> $LOG 2>&1"
-pgrep -f $FILENAME_LOCAL || eval $RUN_COMMAND
+while true; do
+  pgrep -f $FILENAME_LOCAL || eval $RUN_COMMAND &
+  date >> $LOG_PING
+  echo $FILENAME_LOCAL >> $LOG_PING
+  sleep 60s
+done
